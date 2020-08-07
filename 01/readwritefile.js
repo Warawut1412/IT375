bb6fa490b56a055ded3f40d5188c2b20a4068793
Warawut1412 <610108020002@dpu.ac.th>
2020-08-07 15:21:19 +0700

    ReadWriteFile

---
+++ 01/readwritefile.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const textIn = fs.readFileSync('./txt/input.txt','utf-8');
+console.log(textIn);
+// Write File
+const textOut = `Avocado information:${textIn}\nCreat on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt',textOut);
+console.log('File Writte');
+
